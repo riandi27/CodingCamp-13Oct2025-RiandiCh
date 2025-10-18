5e5b41b386e08de4834b19c7ab2d09f2a133da17
riandi27 <riandi.chendana@gmail.com>
2025-10-18 11:01:50 +0700

    Konfigurasi Mini Project RevoU

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function addTask() {
+    const taskInput = document.getElementById('todo-input');
+    const taskDate = document.getElementById('todo-date');
+
+    console.log('Task:', taskInput.value);
+    console.log('Due Date:', taskDate.value);
+}
+
+function deleteAll
+
+function filterTasks
+
+function validateInput 
